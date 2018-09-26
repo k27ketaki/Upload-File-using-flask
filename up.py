66da190cc7cf777fa61ff8b748d93f6e50112e55
Ketaki Kavatkar <ketaki.kavatkar@gmail.com>
2018-09-26 19:12:30 +0530

    Simple uploading page using Flask

---
+++ up.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = 'static'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "uploads"
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "success"
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run()
